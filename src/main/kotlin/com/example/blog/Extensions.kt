9c4bcc170d2c24d19fa570b1ac55d758935a3ae9
ezura <yuka.ezura@gmail.com>
2020-10-11 21:02:50 +0900

    Implement "article/{slug}"

--- src/main/kotlin/com/example/blog/Extensions.kt
+++ src/main/kotlin/com/example/blog/Extensions.kt
@@ -1,11 +1,11 @@
 package com.example.blog
 
-import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatterBuilder
 import java.time.temporal.ChronoField
 import java.util.*
 
-fun LocalDate.format() = this.format(englishDateFormatter)
+fun LocalDateTime.format() = this.format(englishDateFormatter)
 
 private val daysLookup = (1..31).associate { it.toLong() to getOriginal(it) }
 
